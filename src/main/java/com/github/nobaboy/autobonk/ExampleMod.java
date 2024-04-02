79dc8d35fb3d47393e9cc15d220c4971e69ea12d
GitHub Action <action@github.com>
2024-04-02 19:09:32 +0000

    Initialize template repository proper
    Set package name to com.github.nobaboy.autobonk
    Set project name to AutoBonk
    Set modid to autobonk

--- src/main/java/com/github/nobaboy/autobonk/ExampleMod.java
+++ src/main/java/com/github/nobaboy/autobonk/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.nobaboy.autobonk;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "autobonk", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
